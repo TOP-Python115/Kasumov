9db0ba6b0fb4ccdc6af965c1d3c581b6c03e439a
Gennadiy <renardes@gmail.com>
2022-09-28 19:17:36 +0500

    обновление файлов заданий

--- 04.21/HW_184.py
+++
@@ -1,9 +0,0 @@
-def flattening(l):
-    if l == list():
-        return l
-    if isinstance(l[0], list):
-        return flattening(l[0]) + flattening(l[1:])
-    return l[:1] + flattening(l[1:])
-
-
-print(flattening([[45, "Hello World"], [4786, "js"], [14, "python", 25, 36]]))
